6c40604e64771745a057afc462ab223975c4a4a4
Chad Michel <chadmichel@me.com>
2013-02-28 00:54:59 -0500

    add simple store js to handle repository creation

--- public/javascripts/indexeddbaccessor.js
+++ public/javascripts/indexeddbaccessor.js
@@ -78,77 +78,8 @@ var IndexedDbAccessor ;
 
     IndexedDbAccessor.NotesAccessor = function() {    
         var self = this;
-        var store_name = "notes";
         
-        self.put = function(note, callback) {
-            var tx = IndexedDbAccessor.db.transaction(store_name, "readwrite");
-            var store = tx.objectStore(store_name);
-            var req = store.put(note);
-            
-            if (callback !== null) {
-                req.onsuccess = function(event) {
-                    callback();
-                }
-                req.onerror = function(event) {
-                    callback(event);
-                }
-            }
-        };
-        
-        self.forEach = function(callback, itemCallback) {
-            
-            var tx = IndexedDbAccessor.db.transaction(store_name, "readwrite");
-            var store = tx.objectStore(store_name);            
-            
-            var req = store.openCursor();
-            req.onsuccess = function(event) {
-                if (event !== null && event.target !== null) {
-                    var cursor = event.target.result;
-                    if (cursor !== null) {
-                        if (itemCallback !== null)
-                            itemCallback(cursor);
-                        cursor.continue();
-                    }
-                    else {
-                        if (callback !== null)
-                            callback(null);
-                    }
-                }
-            };
-            req.onerror = function(event) {  
-                if (callback !== null)
-                    callback(event);
-            };
-        };
-
-        self.getAll = function(callback) {
-            if (callback === null)
-                return;
-            
-            var result = [];
-            
-            self.forEach(
-                function(err) {
-                    callback(err, result);    
-                },
-                function(cursor) {
-                    result.push(cursor.value);
-                }               
-            );                        
-        }
-        
-        self.removeAll = function(callback) {            
-            self.forEach(
-                function(err) {
-                    callback(err);
-                },
-                function(cursor) {
-                    cursor.delete();
-                }
-            );                
-        }
-        
-        
+        SimpleStore.create(this, "notes", IndexedDbAccessor.db);
     };
     
 })(IndexedDbAccessor || (IndexedDbAccessor = {}));
