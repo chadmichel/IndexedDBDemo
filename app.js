95a7da9f8abcb06441281b3859fcf3837c5d27e9
chadmichel <chadmichel@me.com>
2013-03-10 21:32:55 -0500

    offline closer

--- app.js
+++ app.js
@@ -30,11 +30,10 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
-app.get('/users', user.list);
 
-app.get("/offline1.appcache", function(req, res){
+app.get("/offline.appcache", function(req, res){
   res.header("Content-Type", "text/cache-manifest");
-  res.end("CACHE MANIFEST");
+  res.sendfile('offline1.appcache')
 });
 
 http.createServer(app).listen(app.get('port'), function(){
